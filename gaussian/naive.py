55a0389e3c300654f4d7d75e4a7dac262a7765c5
daschaich <daschaich@gmail.com>
2020-05-27 21:40:50 +0100

    Convert naive sampling to python3

--- gaussian/naive.py
+++ gaussian/naive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 import sys
 import glob
@@ -14,20 +14,21 @@ import matplotlib.pyplot as plt
 # Bootstrap dat to determine the relative error on the histogram counts
 # in each of Nbins bins (normalized by the first count at x=0)
 
-# !!! Currently only have matplotlib v1.5.2 for python3
-# !!! So stick with python2 (and matplotlib v2.2.3) for now
-
-# Parse argument: random seed
+# Parse arguments: random seed, number of points to sample,
+#                  number of resamplings in bootstrap analysis
+# Initial tests used Npts=100,000
+# www.itl.nist.gov/div898/handbook/eda/section3/bootplot.htm
+# --> Suggests ~500 resamples may be optimal
 if len(sys.argv) < 2:
-  print("Usage:", str(sys.argv[0]), "<RNG seed>")
+  print("Usage:", str(sys.argv[0]), "<RNG seed> <N_samples> <N_bootstrap>")
   sys.exit(1)
 seed = int(sys.argv[1])
+Npts = int(sys.argv[2])
+Nsamples = int(sys.argv[3])
 runtime = -time.time()
 
-# Seed (Mersenne Twister) random number generator
-# Use thread-safe RandomState instead of (global) seed
-# Former should allow multiple independent streams if desired
-prng = np.random.RandomState(seed)
+# Seed default (PCG64) random number generator
+prng = np.random.default_rng(seed)
 
 # Number of bins for histograms
 Nbins = 51
@@ -36,56 +37,52 @@ Nbins = 51
 
 
 # ------------------------------------------------------------------
-# Set up array of numbers with hard-coded length Npts=100,000
+# Set up array of numbers with input length Npts
 # We want P(x) ~ exp[x^2 / 16^2]
 # Numpy sets up  exp[x^2 / 2sigma^2]
 mean = 0.0
 stdev = 16.0
-Npts = int(1e5)
 sigma = stdev / np.sqrt(2.0)
 dat = np.abs(prng.normal(mean, sigma, Npts))
 
 # Manually set up bin list using dat
 maxdat = max([np.ceil(max(dat)), -1.0 * np.floor(min(dat))])
-#print max(dat), np.ceil(max(dat)), min(dat), np.floor(min(dat)), maxdat
+#print(max(dat), np.ceil(max(dat)), min(dat), np.floor(min(dat)), maxdat
 spacing = maxdat / Nbins
 binlist = np.arange(0, maxdat + 1, spacing)
 centers = np.arange(0.5 * spacing, maxdat + 1 - 0.5 * spacing, spacing)
-#print len(binlist), len(centers)    # 52, 51
-#print binlist
-#print centers
+#print(len(binlist), len(centers))   # 52, 51
+#print(binlist)
+#print(centers)
 #sys.exit(0)
 
 # Quick visual check of full data set
 # Use 'density' to compare with P(x)
 # This seems to set the integral to unity
 # --> will have bins with less than 1/Npts = 1e-5...
-#count, bins, ignored = plt.hist(dat, Nbins, density=True,
-#                                histtype='step', hatch='//')
-#norm = 2.0 / (stdev * np.sqrt(np.pi))   # Double to add both +/- sides
-#plt.semilogy(bins, norm * np.exp(-bins**2 / stdev**2),
-#             linewidth=2, color='r')
-#plt.axis([0.0, 60.0, 1e-7, 0.1])
-#plt.xticks(np.arange(0, 61, step=20))
-#title = str(Npts) + ' samples with random seed ' + str(seed)
-#plt.title(title)
-#plt.xlabel('x')
-#plt.ylabel('P(x) ~ exp[-x^2 / 16^2]')
-#outfile = 'naive_check.pdf'
-#plt.savefig(outfile)
+count, bins, ignored = plt.hist(dat, Nbins, density=True,
+                                histtype='step', hatch='//')
+norm = 2.0 / (stdev * np.sqrt(np.pi))   # Double to add both +/- sides
+plt.semilogy(bins, norm * np.exp(-bins**2 / stdev**2),
+             linewidth=2, color='r')
+plt.axis([0.0, 60.0, 1e-7, 0.1])
+plt.xticks(np.arange(0, 61, step=20))
+title = str(Npts) + ' samples with random seed ' + str(seed)
+plt.title(title)
+plt.xlabel('x')
+plt.ylabel('P(x) ~ exp[-x^2 / 16^2]')
+outfile = 'naive_check.pdf'
+plt.savefig(outfile)
 #plt.show()
 
-# References for bootstrapping:
+# Reference for bootstrapping:
 # www.physics.utah.edu/~detar/phys6720/handouts/ftspect/ftspect/node7.html
 # --> The uncertainty is the standard deviation of the distribution
-# www.itl.nist.gov/div898/handbook/eda/section3/bootplot.htm
-# --> Do 500 resamples each selecting Npts points with replacement
-Nsamples = 500
 sample = np.empty_like(dat)   # Npts numbers to be pulled from dat
 counts = np.empty((Nsamples, Nbins), dtype = np.float)
 for i in range(Nsamples):
   # Checked that this covers the full range, with replacement
-  indices = prng.randint(0, high=Npts, size=Npts)
+  indices = prng.integers(0, high=Npts, size=Npts)
   for j in range(Npts):
     sample[j] = dat[indices[j]]
 
@@ -103,9 +100,9 @@ for i in range(Nsamples):
 # The error is the standard deviation of the population, not of the mean
 mean = counts.mean(0)
 err = counts.std(0)
-#print len(mean), len(err)    # 51, 51
-#print mean
-#print err
+#print(len(mean), len(err))   # 51, 51
+#print(mean)
+#print(err)
 #sys.exit(0)
 
 # Set up relative error --- some shenanigans needed to skip NaNs
@@ -120,13 +117,13 @@ for i in range(1, Nbins):   # Try to work around divide-by-zero warnings
     relList.append(err[i] / mean[i])
     # Check absolute deviations in units of bootstrap errors
 #    dev = (mean[i] - np.exp(-centers[i]**2 / stdev**2)) / err[i]
-#    print "%.2g %.4g" % (centers[i], dev)
+#    print("%.2g %.4g" % (centers[i], dev))
 x = np.array(xList)
 dev = np.array(devList)
 relErr = np.array(relList)
-#print len(x), len(dev), len(relErr)
-#print dev
-#print relErr
+#print(len(x), len(dev), len(relErr))
+#print(dev)
+#print(relErr)
 #sys.exit(0)
 # ------------------------------------------------------------------
 
